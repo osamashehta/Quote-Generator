8d5b3343d62a6bbba79b6e7d8be1e039894ea00f
osamashehta <osama.shehtafathi@gmail.com>
2024-11-17 13:58:23 +0200

    initial commit

---
+++ javaScript/index.js
@@ -0,0 +1,248 @@
+var quotes = [
+  {
+    name: "Oscar Wilde",
+    quote: `“Be yourself; everyone else is already taken.”`,
+    image: "images/Oscar Wilde.jpg",
+  },
+  {
+    name: "Frank Zappa",
+    quote: `“So many books, so little time.”`,
+    image: "images/Frank Zappa.jpg",
+  },
+  {
+    name: "Albert Einstein",
+    quote: `“Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.”`,
+    image: "images/Albert Einstein.jpg",
+  },
+  {
+    name: "Marcus Tullius Cicero",
+    quote: `“A room without books is like a body without a soul.”`,
+    image: "images/Marcus Tullius Cicero.jpg",
+  },
+  {
+    name: "Bernard M. Baruch",
+    quote: `“Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.”`,
+    image: "images/Bernard M. Baruch.jpg",
+  },
+  {
+    name: "William W. Purkey",
+    quote: `“You've gotta dance like there's nobody watching, Love like you'll never be hurt, Sing like there's nobody listening, And live like it's heaven on earth.”`,
+    image: "images/William W. Purkey.jpg",
+  },
+  {
+    name: "Dr. Seuss",
+    quote: `“You know you're in love when you can't fall asleep because reality is finally better than your dreams.”`,
+    image: "images/Dr. Seuss.jpg",
+  },
+  {
+    name: "Mahatma Gandhi",
+    quote: `“Be the change that you wish to see in the world.”`,
+    image: "images/Mahatma Gandhi.jpg",
+  },
+  {
+    name: "Albert Camus",
+    quote: `“Don’t walk in front of me… I may not follow Don’t walk behind me… I may not lead Walk beside me… just be my friend”`,
+    image: "images/Albert Camus.jpg",
+  },
+  {
+    name: "Mark Twain",
+    quote: `“If you tell the truth, you don't have to remember anything.”`,
+    image: "images/Mark Twain.jpg",
+  },
+  {
+    name: "Elbert Hubbard",
+    quote: `“A friend is someone who knows all about you and still loves you.”`,
+    image: "images/Elbert Hubbard.jpg",
+  },
+  {
+    name: "Bohdi Sanders",
+    quote: `“Focus on making yourself better, not on thinking that you are better.”`,
+    image: "images/Bohdi Sanders.jpg",
+  },
+  {
+    name: "Rumi",
+    quote: `“Your heart is the size of an ocean. Go find yourself in its hidden depths.”`,
+    image: "images/Rumi.jpg",
+  },
+
+  {
+    name: "Aristophanes",
+    quote: `“Open your mind before your mouth”`,
+    image: "images/Aristophanes.jpg",
+  },
+
+  {
+    name: "أحمد شوقي",
+    quote: `“وَكُن في الطَريقِ عَفيفَ الخُطا شَريفَ السَماعِ كَريمَ النَظَر وَلا تَخلُ مِن عَمَلٍ فَوقَهُ تَعِش غَيرَ عَبدٍ وَلا مُحتَقَر”`,
+    image: "images/أحمد شوقي.jpg",
+  },
+  {
+    name: "أحمد شوقي",
+    quote: `“وما نيل المطالب بالتمني ... ولكن تؤخذ الدنيا غِلابا”`,
+    image: "images/أحمد شوقي.jpg",
+  },
+
+  {
+    name: " أبو تمام",
+    quote: `“رَأَيتُ الحُرَّ يَجتَنِبُ المَخازي وَيَحميهِ عَنِ الغَدرِ الوَفاءُ وَما مِن شِدَّةٍ إِلّا سَيَأتي لَها مِن بَعدِ شِدَّتِها رَخاءُ”`,
+    image: "images/أبو تمام.jpg",
+  },
+
+  {
+    name: " علي بن أبي طالب ",
+    quote: `“أَفادَتني القَناعَةُ كُلَّ عِزٍّ وَهَل عِزٌّ أَعَزُّ مِنَ القَناعَة فَصَيِّرها لِنَفسِكَ رَأسُ مالٍ وَصَيِّر بَعدَها التَقوى بِضاعَه”`,
+    image: "images/علي بن أبي طالب.png",
+  },
+  {
+    name: " الإمام الشافعي",
+    quote: `“اِصبِر عَلى مُرِّ الجَفا مِن مُعَلِّمٍ فَإِنَّ رُسوبَ العِلمِ في نَفَراتِهِ وَمَن لَم يَذُق مُرَّ التَعَلُّمِ ساعَةً تَذَرَّعَ ذُلَّ الجَهلِ طولَ حَياتِهِ”`,
+    image: "images/الإمام الشافعي.png",
+  },
+  {
+    name: " الإمام الشافعي",
+    quote: `“صبراً جميلاً ما أقرب الفرجا .. من راقب الله في الأمور نجا
+من صَدقَ الله لم ينله أذى .. و من رجاه يكون حيث رجا”`,
+    image: "images/الإمام الشافعي.png",
+  },
+  {
+    name: " الإمام الشافعي",
+    quote: `“رَأيْتُ القنَاعَة َ رَأْسَ الغنَى فصِرتُ بأَذْيَالِهَا مُمْتَسِكْ فلا ذا يراني على بابهِ وَلا ذا يَرَاني بهِ مُنْهمِكْ”`,
+    image: "images/الإمام الشافعي.png",
+  },
+  {
+    name: "Goethe J.W",
+    quote: `“Everything is hard before it is easy”`,
+    image: "images/Goethe J.W.jpg",
+  },
+  {
+    name: "Samuel Palmer",
+    quote: `“Keep your mouth shut and your eyes open.”`,
+    image: "images/Samuel Palmer.jpg",
+  },
+  {
+    name: "Henry David Thoreau",
+    quote: `“The cost of a thing is that amount of life which must be exchanged for it.”`,
+    image: "images/Henry David Thoreau.jpg",
+  },
+  {
+    name: "William Penn",
+    quote: `“No pain, no palm; no thorns, no throne; no gall, no glory; no cross, no crown.”`,
+    image: "images/William Penn.jpg",
+  },
+  {
+    name: "Aristotle",
+    quote: `“The roots of education are bitter, but the fruit is sweet.”`,
+    image: "images/Aristotle.jpg",
+  },
+  {
+    name: "Abraham Lincoln",
+    quote: `“I have simply tried to do what seemed best each day, as each day came.”`,
+    image: "images/Abraham Lincoln.jpg",
+  },
+
+  {
+    name: "Albert Einstein",
+    quote: `“Life is like riding a bicycle. To keep your balance, you must keep moving.”`,
+    image: "images/Albert Einstein.jpg",
+  },
+
+  {
+    name: "Steve Jobs",
+    quote: `“The only way to do great work is to love what you do.”`,
+    image: "images/Steve Jobs.jpg",
+  },
+  {
+    name: "Nelson Mandela",
+    quote: `“It always seems impossible until it’s done.”`,
+    image: "images/Nelson Mandela.jpg",
+  },
+
+  {
+    name: "Confucius",
+    quote: `“It does not matter how slowly you go as long as you do not stop.”`,
+    image: "images/Confucius.jpg",
+  },
+  {
+    name: "Theodore Roosevelt",
+    quote: `“Believe you can and you’re halfway there.”`,
+    image: "images/Theodore Roosevelt.jpg",
+  },
+
+  {
+    name: "Henry Ford",
+    quote: `“Whether you think you can, or you think you can’t – you’re right.”`,
+    image: "images/Henry Ford.jpg",
+  },
+
+  {
+    name: "Benjamin Franklin",
+    quote: `“Energy and persistence conquer all things.”`,
+    image: "images/Benjamin Franklin.jpg",
+  },
+
+  {
+    name: "Dr. Seuss",
+    quote: `“You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose.”`,
+    image: "images/Dr. Seuss.jpg",
+  },
+  {
+    name: "Mark Twain",
+    quote: `“The secret of getting ahead is getting started.”`,
+    image: "images/Mark Twain.jpg",
+  },
+  {
+    name: "Vince Lombardi",
+    quote: `“Winners never quit and quitters never win.”`,
+    image: "images/Vince Lombardi.jpg",
+  },
+
+  {
+    name: "Thomas Edison",
+    quote: `“Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.”`,
+    image: "images/Thomas Edison.jpg",
+  },
+];
+
+var content = "";
+
+var number = Math.trunc(Math.random() * quotes.length);
+
+content = `<p id="quote" class="fw-bold w-100">
+            ${quotes[number].quote}
+          </p>
+          <div id="author" class=" d-flex  align-items-center">
+            
+            <div class="img  me-3">
+              <img src="${quotes[number].image}"  alt="Oscar Wilde" class="w-100 ">
+            </div>
+            <span id="author-name">
+            ${quotes[number].name}
+            </span>
+          </div>
+  `;
+document.getElementById("author").innerHTML = content;
+var previous = number;
+
+document.querySelector(".c-btn").addEventListener("click", function () {
+  number = Math.trunc(Math.random() * quotes.length);
+
+  if (number == previous) {
+    number = Math.trunc(Math.random() * quotes.length);
+  } else {
+    content = `<p id="quote" class="fw-bold w-100">
+            ${quotes[number].quote}
+          </p>
+          <div id="author" class=" d-flex  flex-wrap align-items-center">
+            
+            <div class="img  me-3">
+              <img src="${quotes[number].image}"  alt="${quotes[number].name}" class="w-100 ">
+            </div>
+            <span id="author-name"  >
+            ${quotes[number].name}
+            </span>
+          </div>
+  `;
+    document.getElementById("author").innerHTML = content;
+  }
+  previous = number;
+});
